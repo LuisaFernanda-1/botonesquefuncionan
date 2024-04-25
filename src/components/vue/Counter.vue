d528eb3e405d272d399dfcf50e502aca671a3747
LuisaFernanda1 <lfovallosc@ufpso.edu.co>
2024-04-25 13:00:11 -0500

    Veo

---
+++ src/components/vue/Counter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue';
+let count=ref(0);
+function addValue() {
+    count.value++;
+    console.log(count)
+    
+}
+</script>
+
+<template>
+  <div @click="addValue"  class="bg-blue-500 text-white py-2 px-4 rounded  inline-block  hover:cursor-pointer transition-colors">
+    ¡Golpeame!{{count}}
+  </div>
+</template>
